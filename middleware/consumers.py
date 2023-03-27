adcc42f4a171090dc546c709f9a5eb7e39549c8b
Ivan Hribernik <cihribernik@gmail.com>
2023-03-27 20:47:49 -0300

    feat: chage Product consumer to work async

--- middleware/consumers.py
+++ middleware/consumers.py
@@ -1,31 +1,51 @@
+import asyncio
 import json
 
-from channels.generic.websocket import WebsocketConsumer
 import requests
+from channels.generic.websocket import AsyncWebsocketConsumer
 from decouple import config
 
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
+class ProductConsumer(AsyncWebsocketConsumer):
+    start = False
+    current_id = ""
+    user = config("HTTP_API_USER")
+    password = config("HTTP_API_PASSWORD")
+    url = config("HTTP_API_URL")
 
-    def disconnect(self, close_code):
-        pass
-
-    def receive(self, text_data):
+    async def receive(self, text_data=None, bytes_data=None):
         text_data_json = json.loads(text_data)
         match text_data_json["type"]:
             case "get_info":
-                user = config("HTTP_API_USER")
-                password = config("HTTP_API_PASSWORD")
-                auth_tuple = (user, password)
-                url = config("HTTP_API_URL")
-                result = requests.get(url, auth=auth_tuple)
-                self.send(text_data=json.dumps({"message": result.json()}))
+                action = text_data_json["action"]
+                self.current_id = text_data_json["id"]
+                if action == "start":
+                    self.start = True
+                else:
+                    self.start = False
 
+                await self._main_loop()
             case _:
-                self.send(
+                self.start = False
+                await self.send(
                     text_data=json.dumps(
                         {"message": "estas preguntando cualquier gilada "}
                     )
                 )
+
+    async def _main_loop(self):
+        while True:
+            if self.start is True:
+                auth_tuple = (self.user, self.password)
+                if self.current_id:
+                    self.url = f"{self.url}/{self.current_id}"
+
+                result = requests.get(self.url, auth=auth_tuple)
+
+                await self.send(
+                    text_data=json.dumps({"message": result.json(), "status": "start"})
+                )
+
+            else:
+                await self.send(text_data=json.dumps({"message": [], "status": "stop"}))
+            await asyncio.sleep(10)
--- middleware/routing.py
+++ middleware/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from middleware import consumers
 
 websocket_urlpatterns = [
-    re_path(r"^ws/$", consumers.ChatConsumer.as_asgi())
+    re_path(r"^ws/$", consumers.ProductConsumer.as_asgi())
 ]
